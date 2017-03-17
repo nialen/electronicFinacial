15456d600dd83bc5d550bf96bb2c6c22260e32b7
dingshaohua <345561171@qq.com>
2017-03-17 15:56:56 +0800

    todo: postMessage

--- resources/js/httpConfig.js
+++ resources/js/httpConfig.js
@@ -4,6 +4,7 @@ define(function () {
         'requestHeader': {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
+        isDevEnvironment: true, //本地开发环境
         isMock: true //是否开启测试数据
     };
     return httpConfig;
