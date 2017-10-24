cd4153c6723caccd819e76aaa8f46e18f46a2898
dingshaohua <345561171@qq.com>
2017-10-24 14:14:58 +0800

    code format

--- entry.js
+++ entry.js
@@ -4,43 +4,43 @@
  */
 
 require.config({
-    paths: {
-        'angular': '/resources/js/angular.min',
-        'angular-touch': '/resources/js/angular-touch.min',
-        'angular-animate': '/resources/js/angular-animate.min',
-        'angular-aria': '/resources/js/angular-aria.min',
-        'jquery': '/resources/js/jquery.min',
-        'bootstrap': '/resources/js/bootstrap.min',
-        'ui-bootstrap': '/resources/js/ui-bootstrap',
-        'ui-bootstrap-tpls': '/resources/js/ui-bootstrap-tpls-2.1.3',
-        'sweetalert': '/resources/js/sweetalert.min',
-        'httpConfig': '/resources/js/httpConfig'
+  paths: {
+    'angular': '/resources/js/angular.min',
+    'angular-touch': '/resources/js/angular-touch.min',
+    'angular-animate': '/resources/js/angular-animate.min',
+    'angular-aria': '/resources/js/angular-aria.min',
+    'jquery': '/resources/js/jquery.min',
+    'bootstrap': '/resources/js/bootstrap.min',
+    'ui-bootstrap': '/resources/js/ui-bootstrap',
+    'ui-bootstrap-tpls': '/resources/js/ui-bootstrap-tpls-2.1.3',
+    'sweetalert': '/resources/js/sweetalert.min',
+    'httpConfig': '/resources/js/httpConfig'
+  },
+  shim: {
+    'angular': {
+      'exports': 'angular'
     },
-    shim: {
-        'angular': {
-            'exports': 'angular'
-        },
-        'angular-touch': {
-            'deps': ['angular'],
-            'exports': 'ngTouch'
-        },
-        'angular-animate': {
-            'deps': ['angular'],
-            'exports': 'ngAnimate'
-        },
-        'angular-aria': {
-            'deps': ['angular'],
-            'exports': 'ngAria'
-        },
-        'bootstrap': {
-            'deps': ['jquery']
-        },
-        'ui-bootstrap-tpls': {
-            'deps': ['angular']
-        }
+    'angular-touch': {
+      'deps': ['angular'],
+      'exports': 'ngTouch'
+    },
+    'angular-animate': {
+      'deps': ['angular'],
+      'exports': 'ngAnimate'
+    },
+    'angular-aria': {
+      'deps': ['angular'],
+      'exports': 'ngAria'
+    },
+    'bootstrap': {
+      'deps': ['jquery']
+    },
+    'ui-bootstrap-tpls': {
+      'deps': ['angular']
     }
+  }
 });
 
-require(['angular', 'index'], function (angular) {
-    angular.bootstrap(document, ['indexModule']);
+require(['angular', 'index'], function(angular) {
+  angular.bootstrap(document, ['indexModule']);
 });
